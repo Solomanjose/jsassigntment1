6e558a42b0670ac76119e96ee2b7cc3f9cb5fe1e
SolomanJose <solomanjose11@gmail.com>
2024-06-04 18:02:29 +0530

    pgm2 done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("alert")
+//1. Create a program that asks the user for two numbers and prints their sum.
+
+let a=2
+let b=3
+let sum=a+b
+console.log("program1 answer.="+sum);
+
+
+// 2.Implement a program that calculates the area of a rectangle using given length and width.
+
+let length=10
+let breadth=20
+let area=length*breadth
+console.log("program 2 answer.area=" +area );
+
+//3.Create a program that asks the user for their age and determines if they are eligible to vote (considering a voting age of 18).
+
